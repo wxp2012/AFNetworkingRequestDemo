714f57478893d53e4bf7bc1fc71c4990eeeaa0c2
xp mac <xp_mac@xp-macdemini.lan>
2016-03-29 10:52:58 +0800

    Initial Commit

---
+++ AFNetworkingRequestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingRequestDemo
+//
+//  Created by xp_mac on 16/3/29.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNetworkingRequestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNetworkingRequestDemo
+//
+//  Created by xp_mac on 16/3/29.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

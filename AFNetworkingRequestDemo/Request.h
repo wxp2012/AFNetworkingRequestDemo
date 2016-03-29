4660effd2e7d816e117d35412a74a021d11ef3de
xp_mac <xp_mac@xp-macdeMini.lan>
2016-03-29 14:26:04 +0800

    first commit

---
+++ AFNetworkingRequestDemo/Request.h
@@ -0,0 +1,13 @@
+//
+//  Request.h
+//  AFNetworkingRequestDemo
+//
+//  Created by xp_mac on 16/3/29.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Request : NSObject
+
+@end
---
+++ AFNetworkingRequestDemo/Request.m
@@ -0,0 +1,13 @@
+//
+//  Request.m
+//  AFNetworkingRequestDemo
+//
+//  Created by xp_mac on 16/3/29.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import "Request.h"
+
+@implementation Request
+
+@end
